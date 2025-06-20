4eba8430773a5d3e198537534dbaa8646eb70140
kingpin <kingpinfx1@gmail.com>
2025-06-20 22:42:13 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by kingpin on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
